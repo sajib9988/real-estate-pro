e45e698fc2a0a07009cac63f6787c1426e621857
sajib9988 <sajib.biswas9988@gmail.com>
2025-07-04 14:37:50 +0600

    uu

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  // আপনার অন্যান্য কনফিগারেশন এখানে থাকতে পারে...
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
--- client/src/service/properties/index.ts
+++ client/src/service/properties/index.ts
@@ -33,3 +33,75 @@ export const addProperties = async (Data: FormData) => {
     throw new Error(`Operation failed: ${error.message || error}`);
   }
 };
+
+export const getProperties = async () => {
+  try {
+     const accessToken = (await cookies()).get("accessToken")?.value;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/properties/`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken as string}`,
+      },
+      next: { tags: ["PROPERTY"] },
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.error('Server responded with error:', errorData);
+      throw new Error(errorData.error || errorData.detail || 'Failed to fetch properties');
+    }
+
+    return res.json();
+  } catch (error: any) {
+    throw new Error(`Operation failed: ${error.message || error}`);
+  }
+};
+
+export const updateProperty = async (id: string, Data: FormData) => {
+  try {
+    const accessToken = (await cookies()).get("accessToken")?.value;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/properties/${id}`, {
+      method: "PUT",
+      body: Data,
+      headers: {
+        Authorization: `Bearer ${accessToken as string}`,
+      },
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.error('Server responded with error:', errorData);
+      throw new Error(errorData.error || errorData.detail || 'Failed to update property');
+    }
+
+    revalidateTag("PROPERTY");
+
+    return res.json();
+  } catch (error: any) {
+    throw new Error(`Operation failed: ${error.message || error}`);
+  }
+}
+
+export const deleteProperty = async (id: string) => {
+  try {
+    const accessToken = (await cookies()).get("accessToken")?.value;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/properties/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${accessToken as string}`,
+      },
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.error('Server responded with error:', errorData);
+      throw new Error(errorData.error || errorData.detail || 'Failed to delete property');
+    }
+
+    revalidateTag("PROPERTY");
+
+    return res.json();
+  } catch (error: any) {
+    throw new Error(`Operation failed: ${error.message || error}`);
+  }
+};
